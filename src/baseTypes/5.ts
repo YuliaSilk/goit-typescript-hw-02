cdf1982f3a4be8f598c46cfac2bd56dfce199384
YuliaSilk <129064716+YuliaSilk@users.noreply.github.com>
2023-11-26 16:43:23 +0200

    my first code in typescript

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,19 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+// let union;
+// let literal;
+
+
+let union: string | number ;
+union = 'string'; 
+union = 10; 
+
+type EnableOrDisable = 'enable' | 'disable';
+let literal: EnableOrDisable ;
+literal = 'enable';   
+literal = 'disable';  
+
+export {};
+
+
